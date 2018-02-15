d2aa46d52e0e67b5a55cd6cc54b90d8b52e336b3
Eleyka <eleynekrd@gmail.com>
2018-02-15 10:45:33 -0500

    Actualizando código

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
 
   if (navigator.geolocation) {
@@ -11,21 +11,36 @@ $(document).ready(function() {
       var longitudApi = myPosition.lng;
 
       var newApilinkDS = `https://api.darksky.net/forecast/ed1d3c87046153f1a88b95d69989b0af/${latitudeApi},%20${longitudApi}?lang=es`;
-     
+
       var proxy = 'https://cors-anywhere.herokuapp.com/';
 
 
       $.ajax({
         url: proxy + newApilinkDS,
-        success:function(data) { console.log(data)
+        success: function (data) {
+          console.log(data)
+          var temperature = data.currently.temperature;
+          console.log(temperature)
+          var wind = data.currently.windSpeed;
+          console.log(wind);
+          var pressure = data.currently.pressure;
+          console.log(pressure)
+          var uv = data.currently.uvIndex
+          console.log(uv)
+          var humidity = data.currently.humidity;
 
-          
+          $('.wind').append(wind);
+          $('.humidity').append(humidity);
+          $('.uv').append(uv);
+          $('.pressure').append(pressure);
 
-          
-          ;}
+
+
+          ;
+        }
       });
-      
-     
+
+
     });
   }
 
