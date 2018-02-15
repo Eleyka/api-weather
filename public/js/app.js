362f6142d9038288c0494a8ebc9c8da7faf41267
SIlviaTivi <skfl.erg@hotmail.com>
2018-02-15 10:36:57 -0500

    subiendo archivo

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let myPosition = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      var latitudeApi = myPosition.lat;
+      var longitudApi = myPosition.lng;
+
+      var newApilinkDS = `https://api.darksky.net/forecast/ed1d3c87046153f1a88b95d69989b0af/${latitudeApi},%20${longitudApi}?lang=es`;
+     
+      var proxy = 'https://cors-anywhere.herokuapp.com/';
+
+
+      $.ajax({
+        url: proxy + newApilinkDS,
+        success:function(data) { console.log(data)
+
+          
+
+          
+          ;}
+      });
+      
+     
+    });
+  }
+
+
+
+
+
+
+});
